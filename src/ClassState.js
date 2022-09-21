d7fa6fd52675dc51573c1fa65fadc9fbba503ab1
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2022-09-21 12:28:27 -0500

    new project secure code : add UseState and ClassState components

---
+++ src/ClassState.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const ClassState = () => {
+	return (
+		<div>
+			<h2>Eliminar ClassState</h2>
+			<p>Por favor escribe el codigo de seguridad</p>
+			<input placeholder="Codigo de seguridad" />
+			<button>Comprobar</button>
+		</div>
+	)
+}
---
+++ src/UseState.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const UseState = () => {
+	return (
+		<div>
+			<h2>Eliminar USeState</h2>
+			<p>Por favor escribe el codigo de seguridad</p>
+			<input placeholder="Codigo de seguridad" />
+			<button>Comprobar</button>
+		</div>
+	)
+}
