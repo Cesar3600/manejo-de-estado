a720ffc576e584522b81f6da32ed08611fb0f742
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2022-09-21 22:06:59 -0500

    vid#4 el uso de useState

--- src/UseState.js
+++ src/UseState.js
@@ -1,14 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const UseState = ({ name }) => {
 	const [error, setError] = useState(false)
+	const [loading, setLoading] = useState(false)
+	//debugger
+
+	useEffect(() => {
+		console.log('inicio  efecto')
+		//debugger
+
+		setTimeout(() => {
+			console.log('me demore 3 segundos?')
+			if (!loading) {
+				console.log('soy ua falsa de 3 segundos')
+			}
+			if (!!loading) {
+				console.log('haciendo la validacion')
+				setLoading(false)
+				console.log('terminando la validacion')
+				//debugger
+			}
+		}, 3000)
+
+		console.log('fin efecto')
+		//debugger
+	}, [loading])
+
+	//debugger
+
 	return (
 		<div>
 			<h2>Eliminar {name}</h2>
 			<p>Por favor escribe el codigo de seguridad</p>
 			<input placeholder="Codigo de seguridad" />
-			<button onClick={() => setError(!error)}>Comprobar</button>
+			<button onClick={() => setLoading(true)}>Comprobar</button>
 			{error && <h4>Error: ingrese su codigo de seguridad!</h4>}
+			{loading && <p>Cargando ...</p>}
 		</div>
 	)
 }
